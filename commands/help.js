7798d6b51b5285191adc791fbdc68aa942bb717c
Arino Jenynof <arino.j17@gmail.com>
2020-09-30 12:57:02 +0700

    Add cooldown
    Signed-off-by: Arino Jenynof <arino.j17@gmail.com>

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,13 @@
 module.exports = {
 	name: "help",
 	description: "Every command loplep-bot supports",
+	usage: "--help",
+	cooldown: 10,
 	execute: (message) => {
 		const reply = [];
 		reply.push(`${message.author}`);
-		reply.push("--today\tShow what happened today in LoveLive! history");
+		// reply.push("--today\tShow what happened today in LoveLive! history");
+		reply.push("**Ngetes cooldown asu**");
 		message.channel.send(reply);
 	}
 }
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const command_files = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 
+const cooldowns = new Discord.Collection();
+
 for (const file of command_files) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
@@ -22,6 +24,23 @@ client.on("message", (message) => {
 		return;
 	
 	const command = client.commands.get(command_name);
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldown_amount = (command.cooldown || 10) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expiration = timestamps.get(message.author.id) + cooldown_amount;
+		if (now < expiration) {
+			const left = (expiration - now) / 1000;
+			return message.channel.send(`${message.author}\nPlease wait ${left.toFixed(1)} second(s)`);
+		}
+	}
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldown_amount);
+
 	try {
 		command.execute(message, args);
 	} catch (error) {
