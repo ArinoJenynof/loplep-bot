e5aa0f239453f02824903908f2d375c2129198c7
Arino Jenynof <arino.j17@gmail.com>
2020-11-12 01:38:00 +0700

    Reorganising
    Signed-off-by: Arino Jenynof <arino.j17@gmail.com>

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,25 @@
-import { marker, author, random, colours } from "../config/bot.js";
-const reply = {}
+import { marker, createEmbed } from "../config/bot.js";
 
 export const command = {
 	name: "help",
-	description: "Show command's function",
+	description: "Show command usage",
 	usage: "--help [command]",
 	execute: (message, args) => {
-		reply["color"] = colours[random(colours.length)];
-		reply["author"] = author;
+		const embed = createEmbed();
 		if (message.client.commands.has(args[0])) {
 			const command = message.client.commands.get(args[0]);
-			reply["title"] = `\`${command.usage}\``;
-			reply["description"] = `${command.description}`;
+			embed["title"] = `\`${command.usage}\``;
+			embed["description"] = `${command.description}`;
 		} else {
-			reply["title"] = "Supported Commands";
-			reply["description"] = "Use `--help [command]` to know command's function ^_^\n\n";
-			reply["description"] += [...message.client.commands.keys()].map(el => `\`${marker}${el}\``).join(", ");
+			embed["title"] = "**A simple Discord bot**";
+			embed["description"] = "Use `--help [command]` for detailed usage ^_^";
+			embed["fields"] = [
+				{
+					name: "Commands",
+					value: [...message.client.commands.keys()].map(el => `\`${marker}${el}\``).join(", ")
+				}
+			];
 		}
-		message.channel.send(`${message.author}\n`, { embed: reply });
+		message.channel.send(`${message.author}`, { embed });
 	}
 }
--- commands/predb.js
+++ commands/predb.js
@@ -1,25 +1,22 @@
-import { author, random, colours } from "../config/bot.js";
+import { createEmbed } from "../config/bot.js";
 import axios from "axios";
-
-const reply = {};
-reply["author"] = author;
-reply["title"] = "PC Release Database";
-reply["fields"] = [];
 
 export const command = {
 	name: "predb",
 	description: "Scene releases database",
-	usage: "--predb [?count]",
+	usage: "--predb [count]",
 	execute: async (message, args) => {
+		const embed = createEmbed();
+		embed["title"] = "PC Releases Database";
+		embed["fields"] = [];
 		const count = args[0] || 20;
 		const response = await axios.get(`https://predb.ovh/api/v1/?q=@cat%20GAMES-PC&count=${count}`);
 		for (const row of response.data.data.rows) {
-			reply["fields"].push({
+			embed["fields"].push({
 				name: row["name"],
 				value: new Date(row["preAt"] * 1000).toISOString()
 			});
 		}
-		reply["color"] = colours[random(colours.length)];
-		message.channel.send(`${message.author}`, { embed: reply });
+		message.channel.send(`${message.author}`, { embed });
 	}
 }
