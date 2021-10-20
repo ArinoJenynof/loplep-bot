6df94b1ada413a5c2f4c7182a1189b1f0004190f
Arino Jenynof <arino.j17@gmail.com>
2021-10-20 23:37:07 +0700

    Initial work for slash commands

--- src/helper/command-aggregator.js
+++
@@ -1,3 +0,0 @@
-export { default as predb } from "../commands/predb.js";
-export { default as birthdays } from "../commands/birthdays.js";
-export { default as help } from "../commands/help.js";
--- src/helper/embed-builder.js
+++
@@ -1,4 +0,0 @@
-import { MessageEmbed } from "discord.js";
-import { author, colours } from "../config.js";
-
-export const responseEmbed = () => new MessageEmbed().setAuthor(...author).setColor(colours[Math.floor(Math.random() * colours.length)]);
