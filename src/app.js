03be289db95cb2722e6f46729b151558464ffce0
Arino Jenynof <arino.j17@gmail.com>
2022-09-04 12:38:15 +0700

    djs-v14: variable names

--- src/app.js
+++ src/app.js
@@ -1,39 +1,25 @@
-import { Client, Collection, Intents } from "discord.js";
-import commands from "./loader.js";
+import { Client, GatewayIntentBits } from "discord.js";
+import { predb } from "./commands/predb.js";
+import { birthdays } from "./commands/birthdays.js";
 
-const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-	presence: {
-		activities: [{
-			name: "/help",
-			type: "LISTENING"
-		}]
-	},
-	allowedMentions: {
-		repliedUser: false
-	}
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-Object.entries(commands).forEach(([commandName, commandObj]) => {
-	client.commands.set(commandName, commandObj);
-});
+const commands = {
+	[predb.data.name]: predb,
+	[birthdays.data.name]: birthdays
+};
 
-client.once("ready", (client) => {
-	console.log(`client: ${client.user.username} ready!`);
-});
+client.once("ready", (client) => { console.log(`client: ${client.user.username} ready!`) });
 
 client.on("interactionCreate", async (interaction) => {
-	if (!interaction.isCommand()) return;
+	if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
-	if (!command) return;
-
+	const { commandName } = interaction;
 	try {
-		await command.execute(interaction);
+		await commands[commandName].execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: "There was an error executing that command!", ephemeral: true });
+		await interaction.reply({ content: "Error running slash command!", ephemeral: true });
 	}
 });
 
